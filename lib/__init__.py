33b78a48db90effdec59502e3d9c83361ee00619
ahmedosmanararu-hue <ahmedosmanararu@gmail.com>
2026-06-09 09:57:16 +0300

    Add automation script with API and file output

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,6 @@
 # automation_tool/__init__.py
 
-from .generate_log import fetch_data, write_log_to_file
+from .generate_log import fetch_data, generate_log, write_log_to_file
 
 # Optional: Define what gets imported when someone uses "from automation_tool import *"
-__all__ = ["fetch_data", "write_log_to_file"]
+__all__ = ["fetch_data", "generate_log", "write_log_to_file"]
